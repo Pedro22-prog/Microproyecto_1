996f4e3962b045a1143c5f566a6a7ff6761de762
Pedro22-prog <pedro.jgonzalez73@gmail.com>
2024-05-26 15:11:20 -0400

    Se revisaron arreglaron los errores del JS, y se reviso el HTML y  CSS

--- script/main.js
+++ script/main.js
@@ -1,128 +1,134 @@
+// define las estadisticas y los estados del juego
 let gameStats = {
     gamesPlayed: 0,
     wins: 0,
     rights: 0,
     loses: 0,
     wrongs: 0
-}
+};
+
+// carga los datos del almacenamiento local
 const savedInfo = localStorage.getItem("hangmanStats");
-if (savedInfo){
-    gameStats = JSON.parse(savedInfo)
+if (savedInfo) {
+    gameStats = JSON.parse(savedInfo);
 }
-function updateStats(){
-    document.querySelector(".number-of-games").innerText = gameStats.gamesPlayed; 
+
+// Maneja los datos mostrados
+function updateStats() {
+    document.querySelector(".number-of-games").innerText = gameStats.gamesPlayed;
     document.querySelector(".number-wins").innerText = gameStats.wins;
     document.querySelector(".rights").innerText = gameStats.rights;
-    document.querySelector(".number-loses").innerText = gameStats,loses;
+    document.querySelector(".number-loses").innerText = gameStats.loses;
     document.querySelector(".wrongs").innerText = gameStats.wrongs;
 }
-function updateStatsLocal(){
+
+// Salva las estadisticas locales 
+function updateStatsLocal() {
     gameStats.gamesPlayed++;
     updateStats();
-    localStorage.setItem("hagmanStats", JSON.stringify(gameStats))
+    localStorage.setItem("hangmanStats", JSON.stringify(gameStats));
 }
-function confirmClear(){
-    const confirmation = confirm("Seguro que quieres borrar tus datos?");
-    if (confirmation){
-        clearLocal();
-    }else{
-        alert("Tu historial sigue intacto!");
-    }
+
+// Borra los datos locales y reinicia el juego
+function clearLocal() {
+    localStorage.removeItem("hangmanStats");
+    alert("Se ha eliminado su historial");
+    location.reload();
 }
-function clearLocal(){
-    localStorage.removeItem("hangmanStats")
-    localStorage.removeItem("wins")
-    localStorage.removeItem("rights")
-    localStorage.removeItem("loses")
-    localStorage.removeItem("wrongs")
-    alert("Se ha eliminado su historial")
-    location.reload()
-}
+
 const word = document.querySelector(".word-display");
-const guessesText = document.querySelector(".guesses-text")
-const keyboardDiv = document.querySelector(".heyboard")
-const hangmanImage = document.querySelector(".hangman-box")
-const hangmanModal = document.querySelector(".hangman-modal")
-const playAgain = document.querySelector("button")
-document.querySelector(".download.btn-cn")?.remove();
-let currentW, correctL, wrongtry;
-const maxGuess = 6;
-const reset =() => {
-    correctL = []
-    wrongtry = 0
-    hangmanImage.src = "imagenes/hangman-0.svg"
-    guessesText.innerText = `${wrongtry}/${maxGuess}`;
-    word.innerHTML = currentW
-        .split("")
-        .map(() => `<li class="letter"></li>`)
-        .join("")
-    keyboardDiv
-        .querySelectorAll("button")
-        .forEach((btn) => (btn.disabled = false))
-    hangmanModal.classList.remove("show")
-};
-const getRanWord = () => {
-    const {wordact, hint} = hangmanDB[Math.floor(Math.random() * hangmanDB.length)];
-    currentW = wordact;
-    document.querySelector(".hint-text").innerText = hint;
+const guessesText = document.querySelector(".guesses-text");
+const keyboardDiv = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector(".hangman-image");
+const hangmanModal = document.querySelector(".hangman-modal");
+const playAgain = document.querySelector(".play-again");
+const hintText = document.querySelector(".hint-text b");
+
+// variables del juego
+let currentWord, correctLetters, wrongTries;
+const maxGuesses = 6;
+
+// reinicia el juego
+function reset() {
+    correctLetters = [];
+    wrongTries = 0;
+    hangmanImage.src = "imagenes/hangman-0.svg";
+    guessesText.querySelector("span").innerText = wrongTries;
+    word.innerHTML = "";
+    currentWord.split("").forEach(() => {
+        word.innerHTML += '<li class="letter"></li>';
+    });
+    keyboardDiv.querySelectorAll("button").forEach((btn) => (btn.disabled = false));
+    hangmanModal.classList.remove("show");
+}
+
+// recibe una letra random de la base de datos
+function getRandomWord() {
+    const { wordact, hint } = hangmanDB[Math.floor(Math.random() * hangmanDB.length)];
+    currentWord = wordact;
+    hintText.textContent = hint;
     reset();
     updateStats();
-};
-const gameover = (isVictory) => {
-    const modalText = isVictory? 'Buen trabajo la palabra correcta era:' : 'Buen intento quizas lo logres la proxima vez! La palabra era:';
-    hangmanModal.querySelector("img").src = `imagenes/${isVictory ? "victory":"lost"}.gif`;
-    hangmanModal.querySelector("h4").innerText = isVictory ? "Felicidades" : "Suerte para la procima. Fin del juego";
-    hangmanModal.querySelector("p").innerHTML = `${modalText} <b>${currentW}<b>`;
+}
+
+// final del juego
+function gameOver(isVictory) {
+    const modalText = isVictory ? 'Buen trabajo, la palabra correcta era:' : 'Buen intento, quizás lo logres la próxima vez. La palabra era:';
+    hangmanModal.querySelector(".modal-image").src = `imagenes/${isVictory ? "victory" : "lost"}.gif`;
+    hangmanModal.querySelector(".modal-title").innerText = isVictory ? "Felicidades" : "Suerte para la próxima. Fin del juego";
+    hangmanModal.querySelector(".modal-text").innerHTML = `${modalText} <b>${currentWord}</b>`;
     hangmanModal.classList.add("show");
-    if (isVictory){
+    if (isVictory) {
         gameStats.wins++;
-    }else{
+    } else {
         gameStats.loses++;
     }
     updateStatsLocal();
-};
-const StartGame(button, clickedL){
-    if(currentW.includes(clickedL)){
-        [...currentW].forEach((letter, index) => {
-            if (let === clickedL){
-                correctL.push(letter)
+}
+
+// Maneja la letra que se ha ingresado
+function handleGuess(button, clickedLetter) {
+    if (currentWord.includes(clickedLetter)) {
+        currentWord.split("").forEach((letter, index) => {
+            if (letter === clickedLetter) {
+                correctLetters.push(letter);
                 word.querySelectorAll("li")[index].innerText = letter;
-                word.querySelectorAll("li")[index].classList.add(guessed)
+                word.querySelectorAll("li")[index].classList.add("guessed");
                 gameStats.rights++;
             }
-        })
-    } else{
-        wrongtry++;
-        hangmanImage.src = `imagenes/hangman-${wrongtry}.svg`
+        });
+    } else {
+        wrongTries++;
+        hangmanImage.src = `imagenes/hangman-${wrongTries}.svg`;
         gameStats.wrongs++;
     }
     button.disabled = true;
-    guessesText.innerText = `${wrongtry}/${maxGuess}`;
-    if (wrongtry === maxGuess) return gameover(false);
-    if (correctL.length === currentW.length) return gameover(true);
-};
-for (let i = 97; i <= 122; i++){
+    guessesText.querySelector("span").innerText = wrongTries;
+    if (wrongTries === maxGuesses) return gameOver(false);
+    if (correctLetters.length === currentWord.length) return gameOver(true);
+}
+
+// crea botones del teclado
+for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardDiv.append(button);
-    button.addEventListener("click", (e) => StartGame(e.target, String.fromCharCode(i)));
+    button.addEventListener("click", (e) => handleGuess(e.target, String.fromCharCode(i)));
 }
-getRanWord();
-playAgain.addEventListener("click", getRanWord)
+
+// Inicio del juego
+getRandomWord();
+
+// Boton de jugar otra vez
+playAgain.addEventListener("click", getRandomWord);
+
+// Input del teclado
 document.addEventListener("keydown", (e) => {
-    const select = e.key.toLowerCase()
-    if(/^[a-z]$/.test(select)){
-        const button = Array.from(keyboardDiv.querySelectorAll("button")).find((btn)=>btn.innerText.toLowerCase() === select && !btn.disabled);
-        if (button){
-            StartGame(button, select);
+    const selectedLetter = e.key.toLowerCase();
+    if (/^[a-z]$/.test(selectedLetter)) {
+        const button = Array.from(keyboardDiv.querySelectorAll("button")).find((btn) => btn.innerText.toLowerCase() === selectedLetter && !btn.disabled);
+        if (button) {
+            handleGuess(button, selectedLetter);
         }
-    }
-});
-NodeList.prototype.contains = function (text){
-    return Array.from(this).some((element) => element.innerText.toLowerCase() === text.toLowerCase())  
-}
-document.addEventListener("keydown", (e) => {
-    if(e.key === "Enter" && hangmanModal.classList.contains("show")){
-        getRanWord()
     }
 });
